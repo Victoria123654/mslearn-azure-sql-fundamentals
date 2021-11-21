ac52d77fd0d2a0dd1d64e1e39d082b1056e7e0a4
Victoria123654 <krasikova.vg@gmail.com>
2021-11-21 09:43:57 -0800

    updated data

--- 04-Performance/monitor_and_scale/sqlworkload.cmd
+++ 04-Performance/monitor_and_scale/sqlworkload.cmd
@@ -1,1 +1,1 @@
-ostress.exe -Saw-server<unique_id>.database.windows.net -itopcustomersales.sql -Ucloudadmin -dAdventureWorks -P<password> -n10 -r2 -q
+ostress.exe -Saw-server788023.database.windows.net -itopcustomersales.sql -Ucloudadmin -dAdventureWorks -P845825Vk! -n10 -r2 -q
