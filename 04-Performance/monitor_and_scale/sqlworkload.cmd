97d50db2811debb08d085787d2099c4c3d9e73e9
Victoria123654 <krasikova.vg@gmail.com>
2021-11-21 23:00:45 -0800

    password update

--- 04-Performance/monitor_and_scale/sqlworkload.cmd
+++ 04-Performance/monitor_and_scale/sqlworkload.cmd
@@ -1,1 +1,1 @@
-ostress.exe -Saw-server788023.database.windows.net -itopcustomersales.sql -Ucloudadmin -dAdventureWorks -P845825Vk! -n10 -r2 -q
+ostress.exe -Saw-server788023.database.windows.net -itopcustomersales.sql -Ucloudadmin -dAdventureWorks -PP@ssw0rd! -n10 -r2 -q
